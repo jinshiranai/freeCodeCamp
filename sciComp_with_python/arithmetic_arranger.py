a17fbced24eb3d656b2d6754c912182e1dac6b14
jinshiranai <jin.shiranai@gmail.com>
2020-10-06 18:51:39 +0900

    Update arithmatic_arranger.py (all tests passed)

--- sciComp_with_python/arithmetic_arranger.py
+++ sciComp_with_python/arithmetic_arranger.py
@@ -18,7 +18,7 @@ def arithmetic_arranger(problems, solution=False):
         split_problem = problem.split()
 
         # If operand length is greater than 4, return size error.
-        if len(split_problem[0]) > 4:
+        if len(split_problem[0]) > 4 or len(split_problem[2]) > 4:
             return "Error: Numbers cannot be more than four digits."
         else:
             # Convert string numbers to int values.
@@ -51,15 +51,18 @@ def arithmetic_arranger(problems, solution=False):
     # Compare top line and bottom line length.
     # Adjust smaller with white space until equal with larger.
     for value in range(len(top_line_nums)):
-        while len(top_line_nums[value]) != len(mid_lines[value]):
-            if len(top_line_nums[value])+1 == len(mid_lines[value]):
+        #while len(top_line_nums[value]) != len(mid_lines[value]):
+        if len(top_line_nums[value])+1 == len(mid_lines[value]):
+            while len(top_line_nums[value]) != len(mid_lines[value]):
                 top_line_nums[value] = '  ' + top_line_nums[value]
                 mid_lines[value] = (mid_lines[value][0] + ' ' *
                     (len(top_line_nums[value]) - 1 - len(mid_lines[value][2:]))
                     + mid_lines[value][2:])
-            if len(top_line_nums[value]) < len(mid_lines[value]):
+        elif len(top_line_nums[value]) < len(mid_lines[value]):
+            while len(top_line_nums[value]) != len(mid_lines[value]):
                 top_line_nums[value] = ' ' + top_line_nums[value]
-            if len(top_line_nums[value]) > len(mid_lines[value]):
+        elif len(top_line_nums[value]) > len(mid_lines[value]):
+            while len(top_line_nums[value]) != len(mid_lines[value]):
                 top_line_nums[value] = '  ' + top_line_nums[value]
                 mid_lines[value] = (mid_lines[value][0] + ' ' *
                     (len(top_line_nums[value]) - 1 - len(mid_lines[value][2:]))
@@ -73,15 +76,35 @@ def arithmetic_arranger(problems, solution=False):
         # Create dashed line equal to problem length.
         lines.append('-' * len(top_line_nums[value]))
     
-    print(top_line_nums)
-    print(mid_lines)
-    print(lines)
+    # Format arranged problems for return.
+    top_line = ''
+    mid_line = ''
+    line_line = ''
+    answer_line = ''
 
-    # If solution == True, display answers under dotted line.
+    for item in top_line_nums:
+        top_line = top_line + '    ' + item
+    top_line = top_line[4:]
+
+    for item in mid_lines:
+        mid_line = mid_line + '    ' + item
+    mid_line = mid_line.strip()
+
+    for item in lines:
+        line_line = line_line + '    ' + item
+    line_line = line_line.strip()
+
     if solution:
-        print(answers)
+        for item in answers:
+            answer_line = answer_line + '    ' + item
+        answer_line = answer_line[4:]
 
+    arranged_problems = top_line + '\n' + mid_line + '\n' + line_line
+    if solution:
+        arranged_problems = arranged_problems + '\n' + answer_line
+
+    return arranged_problems
 
 problems = ['50 - 34', '2345 + 6', '70 + 1001', '420 + 69', '42 - 0']
 
-print(arithmetic_arranger(problems))
+print(arithmetic_arranger(problems, True))
