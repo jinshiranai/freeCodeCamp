34f9dd15f06789e0e891002a53df26c082ff653d
jinshiranai <jin.shiranai@gmail.com>
2020-10-08 17:07:15 +0900

    Start FCC sciPy project 2

---
+++ sciComp_with_python/add_time.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+# A function to add time.
+
+def add_time(start_time, added_time, day=''):
+
+    # Parse the start time.
+
+    # Parse the added time.
+
+    # Add the time together.
+
+    # Determine AM PM switch, if any.
+
+    # Determine number of days passed, if any.
+
+    # Determine new day of the week, if provided and changed.
